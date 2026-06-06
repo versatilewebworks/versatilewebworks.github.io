b70c3f407a6513927770100f7a3d013f9e4b705e
Versatile Web Works <256327690+versatilewebworks@users.noreply.github.com>
2026-06-06 18:59:50 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['var(--font-playfair-display)', 'serif'],
+        sans: ['var(--font-inter)', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
