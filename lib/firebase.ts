d4e1448eaf68322da7896de665562586941ec3d0
Versatile Web Works <256327690+versatilewebworks@users.noreply.github.com>
2026-06-07 09:13:50 +0000

    Fixed various authentication bugs

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? '',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? '',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? undefined,
+  apiKey: "AIzaSyAqrPpDQagx3nea1kJeQdz4qp_VaXWllqU",
+  authDomain: "authentication-2a697.firebaseapp.com",
+  projectId: "authentication-2a697",
+  storageBucket: "authentication-2a697.appspot.com",
+  messagingSenderId: "672346373984",
+  appId: "1:672346373984:web:a918528e84683c89d56805",
+  measurementId: "G-17EZ2GBP9E",
 };
 
 const hasFirebaseConfig = Boolean(
